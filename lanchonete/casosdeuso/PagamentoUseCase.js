dc6eded3e5c7aa7affe3d8db9b578345c6d2631c
rodsbg <138177385+rodsbg@users.noreply.github.com>
2024-07-30 22:50:44 -0300

    Update PagamentoUseCase.js
    Corrigir erro

--- lanchonete/casosdeuso/PagamentoUseCase.js
+++ lanchonete/casosdeuso/PagamentoUseCase.js
@@ -1,33 +1,43 @@
 const pagamentoController = require('../interfaces/controladores/PagamentoController');
 
-const listarPagamentos = async(req,res) => {
+const listarPagamentos = async (req, res) => {
   try {
-    
-    const pagamento = await pagamentoController.listarPagamentos();
+    const pagamentos = await pagamentoController.listarPagamentos();
+
+    if (!pagamentos || pagamentos.length === 0) {
+      res.status(404).json({ error: 'Nenhum pagamento realizado' });
+    } else {
+      res.status(200).json(pagamentos);
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+const buscarStatusPgtporcpf = async (req, res) => {
+  try {
+    const { cpf } = req.params;
+    const pagamento = await pagamentoController.buscarStatusPgtporcpf(cpf);
 
     if (!pagamento) {
-      res.status(404).json({ error: 'Nenhum Pagamento realizado' });
+      res.status(404).json({ error: 'Pagamento não encontrado para o CPF fornecido' });
     } else {
       res.status(200).json(pagamento);
     }
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
-}
+};
 
-const buscarStatusPgtporcpf  = async (req, res) => {
+const processarNotificacaoPagamento = async (notification) => {
+  const { id, status, transaction_amount } = notification;
+
   try {
-    const { cpf } = req.params;
-    const pagamento = await pagamentoController.buscarStatusPgtporcpf(categoria);
-    res.json(pagamento);
+    // Lógica para processar a notificação de pagamento
+    await pagamentoController.processarNotificacao(id, status, transaction_amount);
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    console.error('Erro ao processar notificação de pagamento:', error.message);
   }
 };
 
-exports.processarNotificacaoPagamento = async (notification) => {
-  const { id, status, transaction_amount } = notification;
-
-
-module.exports = { listarPagamentos, buscarStatusPgtporcpf  };
-  
+module.exports = { listarPagamentos, buscarStatusPgtporcpf, processarNotificacaoPagamento };
